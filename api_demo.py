e349a813540a642f66637bac469e459cb50b82dd
Kushagra  Srivastav <srivastavkushagra777@gmail.com>
2025-04-30 11:45:42 +0530

    updated demo file
    added new way of initializing kennel

--- api_demo.py
+++ api_demo.py
@@ -17,13 +17,17 @@ from config import ao_apikey, openai_apikey, google_apikey
     #--- We are defining an architecture through api call and we can call this architecture (kennel) to initialize an agent ---#
 url = "https://api.aolabs.ai/v0dev/kennel"
 
-payload = {
+payload = { 
     "kennel_id": "tutorial_kennel",  ## --> give e unique name to kennel, multiple agents can be build on one kennel name. 
-    "arch_URL": "https://raw.githubusercontent.com/aolabsai/ao_loop1/refs/heads/main/demo_arch.py",   ##--> you need to define the architecture like this on github
     "description": "write your discription of kennel",
     "permissions": "private",
-    "email": "kushagra@aolabs.ai"  ## Mendatory for assigning developer id. 
-}
+    "email": "kushagra@aolabs.ai",  ## Mendatory for assigning developer id. 
+    "arch": {
+        "arch_i": "[2, 2, 3]",
+        "arch_z": "[5]",
+        "connector_function": "full_conn"
+    } 
+    }
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
@@ -40,18 +44,12 @@ print(response.text)
 
 url = "https://api.aolabs.ai/v0dev/kennel/agent"
 
-INPUT_AO_api = "1101000"
-LABEL = "10101"  
 payload = {
-    "kennel_id": "tutirial_kennel",  # use kennel_name entered above
-    "agent_id": "tutorial_agent",   # enter unique user IDs here, to call a unique agent for each ID.
-    "INPUT": INPUT_AO_api,  # pass through the input from embedding_bucketing.auto_sort, adding any other inputs
-    "LABEL": LABEL,
-    "email": "kushagra@aolabs.ai", 
-    "control": {
-        "US": True,
-        "states": 5,
-    }
+    "kennel_id": "tutirial_kennel",
+    "agent_id": "tutorial_agent",
+    "email": "kushagra@aolabs.ai",
+    "INPUT": "1101000",
+    "LABEL": "10101"
 }
 headers = {
     "accept": "application/json",
